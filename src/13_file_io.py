a62a2af0fce3a2f53b3cd705679f49d805386b34
John Afolabi <johnafolabi.da@gmail.com>
2020-04-06 18:06:01 +0100

    Complete task 14

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('src/foo.txt') as foo:
+    print(foo.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +19,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('src/bar.txt', 'w') as bar:
+    bar.write(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \nSed mattis mattis sagittis. Nulla quis ligula non quam posuere auctor. \nNunc ultrices faucibus libero, in hendrerit mi laoreet non"
+    )
